d0a459617f6786b723518b69778c578357cdd703
Farhan <kuuhaku.1332@gmail.com>
2022-08-21 05:15:23 +0700

    add:belajar dasar pemrograman web

---
+++ dicoding-course/Belajar Dasar Pemrograman Web/Proyek/assets/js/daftar-matkul.js
@@ -0,0 +1,51 @@
+async function getData() {
+  const response = await fetch("./assets/js/daftar-matkul.json");
+  if (response.status !== 200) {
+    throw new Error("cannot fetch data");
+  }
+  const result = await response.json();
+
+  return result;
+}
+getData().then((result) => {
+  const data = result.values;
+  const eleDaftarMataKuliah = document.getElementById("daftar-mata-kuliah");
+  // console.log(data);
+
+  let item = "";
+  for (let i = 0; i < data.length; i++) {
+    const {
+      kode_mata_kuliah,
+      nama_mata_kuliah,
+      semester,
+      kategori,
+      sks,
+      jenis,
+    } = data[i];
+    item += `<article class="card-matkul">`;
+    item += `<h3 class="category-header">${nama_mata_kuliah}</h3>`;
+    item += `<div class="category">`;
+    item += `<p class="category-item">${sks} SKS</p>`;
+    item += `<p class="category-item">${jenis}</p>`;
+    item += `<p class="category-item">${kategori}</p>`;
+    item += `<p class="category-item">${kode_mata_kuliah}</p>`;
+    item += `<p class="category-item">Semester ${semester}</p>`;
+    item += `</div>`;
+    item += `</article>`;
+  }
+  // console.log(item);
+  // console.log(eleDaftarMataKuliah);
+  eleDaftarMataKuliah.innerHTML = item;
+});
+
+// for (let i = 0; i < 10; i++) {
+//   document.write(`<div class="card">
+//   <h3 class="category-header">Bahasa Inggris</h3>
+//   <div class="category">
+//     <p class="category-item">2 SKS</p>
+//     <p class="category-item">Wajib</p>
+//     <p class="category-item">T</p>
+//     <p class="category-item">N201702</p>
+//   </div>
+// </div>`);
+// }
