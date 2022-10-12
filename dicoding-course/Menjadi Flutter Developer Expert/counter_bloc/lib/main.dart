fcf14d0c56d3e61bc3feccb73605fa2c1d75de62
Farhan <kuuhaku.1332@gmail.com>
2022-10-12 12:22:31 +0700

    add: latihan counter dengan package bloc

--- dicoding-course/Menjadi Flutter Developer Expert/counter_bloc/lib/main.dart
+++ dicoding-course/Menjadi Flutter Developer Expert/counter_bloc/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:counter_bloc/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider<CounterBloc>(
+        create: (_) => CounterBloc(),
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
@@ -37,46 +41,38 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: StreamBuilder(
-          stream: counterBloc.counterStream,
-          builder: (context, snapshot) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
+      body: BlocBuilder<CounterBloc, CounterState>(builder: (context, state) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
               ),
-            );
-          }),
+              Text(
+                '${state.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        );
+      }),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () => counterBloc.eventSink.add(CounterEvent.Decrement),
+            onPressed: () => context.read<CounterBloc>().add(Decrement()),
             tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
           const SizedBox(width: 8),
           FloatingActionButton(
-            onPressed: () => counterBloc.eventSink.add(CounterEvent.Increment),
+            onPressed: () => context.read<CounterBloc>().add(Increment()),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
         ],
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    counterBloc.dispose();
   }
 }
